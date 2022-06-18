8b09502e9b6017bfa8d11b06eca42ab19cafd605
sunil <sunilyadavwork2003@gmail.com>
2022-06-18 09:21:47 +0530

    changed ASGI file

--- InstaClone/asgi.py
+++ InstaClone/asgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InstaClone.settings')
+django.setup()
 
-application = get_asgi_application()
+applicattion=ProtocolTypeRouter({
+    "http":AsgiHandler(),
+})
+
+# application = get_asgi_application()
